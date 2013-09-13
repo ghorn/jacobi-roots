316d2c162ff9b2dd1464451a5023cd1b4d53f60d
Greg Horn <gregmainland@gmail.com>
2013-09-13 13:21:45 +0200

    added readme and doctests, closes #1

---
+++ README.md
@@ -0,0 +1,8 @@
+jacobi-roots
+===
+
+This package provides roots of shifted Radau and Legendre polynomials to double precision.
+
+The roots are precomputed using a Mathematica script, and degrees of up to a few hundred are provided.
+
+[The documentation is on hackage](http://hackage.haskell.org/package/jacobi-roots)
---
+++ src/JacobiRoots.hs
@@ -0,0 +1,65 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Get the roots of shifted Legendre and Radau polynomials
+--
+-- >>> shiftedLegendreRoots 3
+-- Just (fromList [0.11270166537925831,0.5,0.8872983346207417])
+-- >>>  shiftedRadauRoots 2
+-- Just (fromList [0.1550510257216822,0.6449489742783178])
+--
+-- The roots are pre-computed and only a finite number are provided
+--
+-- >>> (V.length allShiftedLegendreRoots, V.length allShiftedRadauRoots)
+-- (301,301)
+-- >>>  shiftedLegendreRoots 1000000000000000
+-- Nothing
+--
+-- There are N roots in the Nth Jacobi polynomial
+--
+-- >>> import Data.Maybe ( fromJust )
+-- >>> V.length (fromJust (shiftedLegendreRoots 5))
+-- 5
+-- >>> all (\k -> k == V.length (fromJust (shiftedLegendreRoots k))) [0..(V.length allShiftedLegendreRoots - 1)]
+-- True
+-- >>> all (\k -> k == V.length (fromJust (shiftedRadauRoots k))) [0..(V.length allShiftedRadauRoots - 1)]
+-- True
+
+module JacobiRoots
+       ( shiftedLegendreRoots
+       , shiftedRadauRoots
+       , allShiftedLegendreRoots
+       , allShiftedRadauRoots
+       ) where
+
+import JacobiRootsRaw ( allShiftedLegendreRoots, allShiftedRadauRoots )
+
+import qualified Data.Vector as V
+import Data.Vector ( (!?) )
+
+-- | get the roots of the Nth shifted Legendre polynomial
+--
+-- @
+-- 'shiftedLegendreRoots' == ('allShiftedLegendreRoots' '!?')
+-- @
+--
+-- >>> mapM_ (print . shiftedLegendreRoots) [0..3]
+-- Just (fromList [])
+-- Just (fromList [0.5])
+-- Just (fromList [0.2113248654051871,0.7886751345948129])
+-- Just (fromList [0.11270166537925831,0.5,0.8872983346207417])
+shiftedLegendreRoots :: Int -> Maybe (V.Vector Double)
+shiftedLegendreRoots = (allShiftedLegendreRoots !?)
+
+-- | get the roots of the Nth shifted Radau polynomial
+--
+-- @
+-- 'shiftedRadauRoots' == ('allShiftedRadauRoots' '!?')
+-- @
+--
+-- >>> mapM_ (print . shiftedRadauRoots) [0..3]
+-- Just (fromList [])
+-- Just (fromList [0.3333333333333333])
+-- Just (fromList [0.1550510257216822,0.6449489742783178])
+-- Just (fromList [8.858795951270394e-2,0.4094668644407347,0.787659461760847])
+shiftedRadauRoots :: Int -> Maybe (V.Vector Double)
+shiftedRadauRoots = (allShiftedRadauRoots !?)
